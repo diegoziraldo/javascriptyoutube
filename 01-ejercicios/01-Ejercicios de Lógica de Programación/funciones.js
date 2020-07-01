6935b85b46d96569b5bac097884ba59e449adba9
ziraldodiego <ziraldodiego@gmail.com>
2020-07-01 14:29:20 -0300

    a

--- 01-ejercicios/01-Ejercicios de Lógica de Programación/funciones.js
+++ 01-ejercicios/01-Ejercicios de Lógica de Programación/funciones.js
@@ -1,13 +1,3 @@
-
-
-
-
-//4) Programa una función que repita un texto X veces, 
-//pe.miFuncion('Hola Mundo', 3), 
-//devolverá Hola Mundo Hola Mundo Hola Mundo.
-
-
-
 'use strict'
 //1) Programa una función que cuente el número de caracteres,
 //de una cadena de texto, pe.miFuncion("Hola Mundo") devolverá 10.
@@ -17,6 +7,7 @@
 //o la cantidad de objetos que tenga una clase
 
 //Contando caracteres
+console.log('1)Programa una función que cuente el número de caracteres, de una cadena de texto, pe.miFuncion("Hola Mundo") devolverá 10.')
 function primerEjercicio(texto){
     texto = texto.length;
     return `La cantidad de caracteres que tiene el texto son: ${texto}`;
@@ -48,6 +39,24 @@ console.log(objetos);
 console.log(`La cantidad de objetos son: ${objetos.length}`);
 console.log(miObjeto);
 
+
+//1)Ejercicio resuelto
+//Aqui vamos a crear una funcion que recibira como parametros,
+//una cadena de texto, que la predefinimos vacia "(texto = "")"
+function contarCaracteres(texto = ""){
+    //Aqui estamos creando el condicional,
+    //que le pondremos el signo exclamacion, 
+    //el cual nos devolvera un valor booleano, este valor booleano,
+    //sera lo contrario a lo que seria realmente,
+    //si la cadena de texto tiene un texto, el booleando seria true,
+    //pero como esta el signo de exclamacion, entonces devuelve false 
+    if(!texto){
+        console.warn('No se ingreso ningun texto')
+    }else{
+        console.info(`El texto ingresado es ${texto}, y la cadena tiene ${texto.length} caracteres`)
+    }
+}
+contarCaracteres()
     
 
 //2) Programa una función que te devuelva el texto recortado,
@@ -60,6 +69,7 @@ console.log(miObjeto);
 
 
 //Contando caracteres de un String
+console.log('2)Programa una función que te devuelva el texto recortado, según el número de caracteres indicados, pe.miFuncion("Hola Mundo", 4) devolverá "Hola".')
 function segundoEjercicio(texto) {
     return texto.slice(0,5);
 }
@@ -91,10 +101,17 @@ let cant = Object.values(miObjeto2)
 console.log(cant.slice(1,3))
 
 
+//Resuelto 2)
+const recortarTexto = (cadena = "", longitud = undefined) =>{
+
+}
+
 //3) Programa una función que dada una String te devuelva un Array,
 //de textos separados por cierto caracter, 
 //pe.miFuncion('hola que tal', ' ') devolverá['hola', 'que', 'tal'].
 
+//Separando palabras de un string a un array
+console.log('3)Programa una función que dada una String te devuelva un Array, de textos separados por cierto caracter, pe.miFuncion("hola que tal", " ") devolverá["hola", "que", "tal"].')
 function tercerEjercicio(texto){
     return texto.split(' ');
 }
@@ -103,3 +120,15 @@ let resultado = tercerEjercicio('hola que tal')
 console.log(resultado)
 console.log(resultado.length);
 
+//4) Programa una función que repita un texto X veces, 
+//pe.miFuncion('Hola Mundo', 3), 
+//devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+//Repitiendo el texto de una cadena
+console.log('4)Programa una función que repita un texto X veces pe.miFuncion("Hola Mundo", 3), devolverá Hola Mundo Hola Mundo Hola Mundo.')
+function cuartoEjercicio(texto) {
+    return texto.repeat(3);
+}
+
+let repeticiones = cuartoEjercicio('Hola Mundo ')
+console.log(repeticiones)
