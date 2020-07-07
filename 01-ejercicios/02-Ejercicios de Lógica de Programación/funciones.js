c4db423f28bed5d9a3cc9caa269f590338bc0793
ziraldodiego <ziraldodiego@gmail.com>
2020-07-06 22:16:16 -0300

    a

--- 01-ejercicios/02-Ejercicios de Lógica de Programación/funciones.js
+++ 01-ejercicios/02-Ejercicios de Lógica de Programación/funciones.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//https://www.youtube.com/watch?v=-JmapiNx7Jc
 
 //5) Programa una función que invierta las palabras de una cadena de texto,
 //pe.miFuncion("Hola Mundo") devolverá "odnuM aloH".
@@ -26,11 +26,11 @@ function reversa(text = ""){
     //este metodo va a hacer lo mismo que split pero al reves,
     //a una cadena de elementos la convertira en un 'String',
     //segun el valor que le hayamos puesto en el parametro del metodo. 
-    else return console.log(text.split("").reverse().join("")); 
+    else return console.log(text.split("").reverse().join(""));
 }
     //Aqui estamos ejecutando la funcion que creamos anteriormente,
     //y como parametros le estamos pasando el texto a dar vuelta.
-    reversa("para bailar la bamba se nececita una poca de gracias, una poca de gracias y una cocita y arriba y arriba");
+    reversa("Para bailar la bamba para bailar la bamba se necesita un poquito de gracia poquito de gracia y una cocita y arriba y arriba y arriba y arriba arriba ire por ti sere por ti sere bamba bamba ");
 
 
 
@@ -44,7 +44,25 @@ function reversa(text = ""){
 
 //6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe.miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
 
+function palabraRepetida(texto, palabra){
+    let palabrasRepetidas;
+    let palabras = texto.split(" ")
+    
+    for(let i=0;i<=palabras.length;i++){
+        if(palabras === palabra){
 
+        parseInt(palabrasRepetidas) = palabrasRepetidas++;
+        //return console.log(`Las palabras repetidas son: ${palabrasRepetidas}`)
+        
+    }else{
+
+    }
+    
+    /* return console.log(`Las palabras repetidas son: ${palabras}`) */
+}
+console.log(palabrasRepetidas)
+}
+palabraRepetida("hola mundo adios mundo", "mundo")
 
 
 
@@ -73,3 +91,4 @@ function reversa(text = ""){
 
 //7) Programa una función que valide si una palabra o frase dada, es un palíndromo(que se lee igual en un sentido que en otro), pe.mifuncion("Salas") devolverá true.
 //8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe.miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá "1, 2, 3, 4 y 5
+//
