119ae98d9cc880c1f35c87bf73b3b29f05b5d967
ziraldodiego <ziraldodiego@gmail.com>
2021-02-12 02:10:54 -0300

    12022021

---
+++ DOM HTML/JS/dom.js
@@ -0,0 +1,15 @@
+/* 		console.log(window);
+		console.log(document);
+
+		let texto = "Buenisimo";
+		const habla = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+
+		habla(texto);
+
+
+		console.log("***************Elementos del documento*****************");
+		console.log(document.images);
+		console.log(document.scripts); */
+
+		
+		
